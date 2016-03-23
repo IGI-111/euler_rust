c646200a4a30bfd89e9adfc1f26efdd1a2f106fd
IGI-111 <igi.222@gmail.com>
2016-03-23 18:08:24 +0100

    solved problem 2

---
+++ fibonacci.rs
@@ -0,0 +1,21 @@
+fn fibonacci(upper_bound: u32) -> Vec<u32> {
+    let mut seq = vec![1];
+    let mut next: u32 = 2;
+    while next < upper_bound {
+        seq.push(next);
+        next = match seq.split_last() {
+            None => panic!("seq is empty"),
+            Some((ultimate, front)) => match front.last() {
+                None => panic!("seq has only one element"),
+                Some(penultimate) => penultimate + ultimate
+            }
+        };
+    }
+    seq
+}
+pub fn main() {
+    let sum: u32 = fibonacci(4000000).iter().fold(0, |a,b|
+                                                 if b & 1 == 0 {a+b}
+                                                 else {a});
+    println!("{:?} {}", fibonacci(4000000), sum);
+}
